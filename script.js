ec8c744b308dc075c385f798e5bf50f13134eff7
viniciusguzzoferreira <148070502+viniciusguzzoferreira@users.noreply.github.com>
2025-03-10 12:13:46 -0300

    ok

--- script.js
+++ script.js
@@ -14,5 +14,10 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
-
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       })
 });
